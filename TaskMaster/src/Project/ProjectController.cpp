2a23cb16f04fe69e06025b1a095bb177112e5136
Maxim Sikanov <max.4251474@yandex.ru>
2023-05-03 15:30:25 +0300

    init test for project module

--- TaskMaster/src/Project/ProjectController.cpp
+++ TaskMaster/src/Project/ProjectController.cpp
@@ -1,1 +1,19 @@
 #include "ProjectController.hpp"
+
+ProjectController::ProjectController(IProjectRepoUptr projectRepo, IBoardRepoUptr boardRepo) : _projectRepo(std::move(projectRepo)), _boardRepo(std::move(boardRepo)) {}
+ProjectController::~ProjectController() {}
+
+void ProjectController::CreateBoard(int projectId, const std::string &boardName)
+{
+}
+void ProjectController::AddUser(int projectId, const std::string &userName)
+{
+}
+std::vector<User> ProjectController::GetAllUsers(int projectId)
+{
+    return std::vector<User>{};
+}
+std::vector<Board> ProjectController::GetAllBoards(int projectId)
+{
+    return std::vector<Board>{};
+}
