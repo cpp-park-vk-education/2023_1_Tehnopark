c4e39c93c9ab4c1bdc36eaf064385570351f20b6
Polina <stepolant@gmail.com>
2023-05-14 19:38:43 +0300

    added stubs for task part

--- TaskMaster/include/Task/TaskController.hpp
+++ TaskMaster/include/Task/TaskController.hpp
@@ -13,9 +13,9 @@ class TaskController : public TaskHandlerInterface
 public:
     TaskController(TRepoUPtr repo): _repo(std::move(repo)) {}
     
-    bool EditTask(const Task& newTask) override;
-    bool CreateTask(const Task& Task) override;
-    virtual bool DeleteTask(int taskId) override;
+    bool EditTask(const Task& newTask) override{return true;}
+    bool CreateTask(const Task& Task) override{return true;}
+    bool DeleteTask(int taskId) override{return true;}
 
     ~TaskController();
 
--- TaskMaster/src/Task/TaskRepo.cpp
+++ TaskMaster/src/Task/TaskRepo.cpp
@@ -1,1 +1,26 @@
 #include "TaskRepo.hpp"
+
+bool EditTask(const Task& newTask)
+{
+    return true;
+}
+    
+bool CreateTask(const Task& Task)
+{
+    return true;
+}
+    
+std::vector<Task> GetAllTasksForBoard(int boardId)
+{
+    return std::vector<Task> ();
+}
+
+bool ChangeTaskStatus(TaskStatus status, int taskId)
+{
+    return true;
+}
+
+bool DeleteTask(int taskId)
+{
+    return true;
+}
