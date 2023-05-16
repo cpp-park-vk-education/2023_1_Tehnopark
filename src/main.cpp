ce5d9d9b59a0bb48fb348000e12e84709b3bf0fe
Stoma Fedor <fedorstoma2@gmail.com>
2023-05-16 14:24:31 +0300

    add working repositories and controllers

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,9 @@
 int main()
 {
     auto db = std::make_shared<DbDriver>("hostaddr=95.165.158.58 port=28009 dbname=TaskMaster user=umlaut-super password=0FNYiW}GwcaSfMh");
-    auto repo = std::make_unique<ProjectRepo>(db);
-    MainPadgeController mp(std::move(repo));
-    std::vector<Project> res = mp.GetUserProjects(1);
-
-    for (const auto &project : res)
-    {
-        std::cout << project.Name << std::endl;
-    }
-    Project pr = mp.GetProjectById(7);
-    std::cout << pr.Name << std::endl;
-     mp.CreateProject(1, "Lll");
+    auto repo = std::make_unique<UserRepo>(db);
+    UserController mp(std::move(repo));
     
+    mp.CreateUserWithIdentity(1);
     return 0;
 }
