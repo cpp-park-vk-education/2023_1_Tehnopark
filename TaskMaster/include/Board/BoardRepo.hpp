bc9969b0ade7baeb4a015338cf4f480a35348f69
Maxim Sikanov <max.4251474@yandex.ru>
2023-05-16 13:02:46 +0300

    fix board

--- TaskMaster/include/Board/BoardRepo.hpp
+++ TaskMaster/include/Board/BoardRepo.hpp
@@ -2,17 +2,17 @@
 #include "DbDriverInterface.hpp"
 #include <memory>
 
-using IDbDriverUptr = std::unique_ptr<DbDriverInterface>;
+using DbDriverSPtr = std::shared_ptr<DbDriverInterface>;
 
 class BoardRepo : public BoardRepoInterface
 {
 public:
     ~BoardRepo() { }
-    BoardRepo(IDbDriverUptr dbBriver) : _dbDriver(std::move(dbBriver)) { }
+    BoardRepo(DbDriverSPtr dbBriver) : _dbDriver(std::move(dbBriver)) { }
     std::vector<Board> GetAllBoardsForProject(int projectId) override;
     bool CreateBoard(int projectId, int creatorId, const std::string &name, const std::string &text) override;
     bool DeleteBoard(int boardId) override;
     Board GetBoard(int boardId) override;
 private:
-    IDbDriverUptr _dbDriver;
+    DbDriverSPtr _dbDriver;
 };
