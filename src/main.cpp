e437df0459061bad10781b4e32ab899769fe49d5
Maxim Sikanov <max.4251474@yandex.ru>
2023-05-16 11:07:40 +0300

    dont wark

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@
 
 int main()
 {
-    auto db = std::make_shared<DbDriver>("hostaddr=192.168.137.1 port=5432 dbname=taskmanager user=postgres password=1474");
+    auto db = std::make_shared<DbDriver>("hostaddr=95.165.158.58 port=28009 dbname=TaskMaster user=umlaut-super password=0FNYiW}GwcaSfMh");
     auto repo = std::make_unique<ProjectRepo>(db);
     MainPadgeController mp(std::move(repo));
-    std::vector<Project> res = mp.GetUserProjects(1);
+    // std::vector<Project> res = mp.GetUserProjects(1);
 
-    for (const auto &project : res)
-    {
-        std::cout << project.Name << std::endl;
-    }
-    Project pr = mp.GetProjectById(7);
-    std::cout << pr.Name << std::endl;
-    // mp.CreateProject(2, "CreateUest");
+    // for (const auto &project : res)
+    // {
+    //     std::cout << project.Name << std::endl;
+    // }
+    // Project pr = mp.GetProjectById(7);
+    // std::cout << pr.Name << std::endl;
+     mp.CreateProject(2, "CreateUest");
     
     return 0;
 }
