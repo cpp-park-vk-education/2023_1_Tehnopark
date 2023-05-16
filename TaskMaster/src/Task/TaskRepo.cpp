75be03efac741e9b042e9f0c794b4b6192a42335
polina <polina@Ubuntu2.myguest.virtualbox.org>
2023-05-16 13:56:26 +0300

    taskRepo is correct now

--- TaskMaster/src/Task/TaskRepo.cpp
+++ TaskMaster/src/Task/TaskRepo.cpp
@@ -4,19 +4,19 @@
 
 bool TaskRepo::EditTask(const Task& newTask)
 {
-    if (!_dr->Connected())
+    if (_dr->Connected())
         return false;
     _dr->Exec("UPDATE db_task SET name=\'" + newTask.Name 
                 + "\', text=\'" + newTask.Text  
-                + "\', status=" + std::to_string(newTask.Status) 
+                + "\', status=" + std::to_string(static_cast<int>(newTask.Status)) 
                 + ", board_id=" + std::to_string(newTask.BoardId)
-                + ", duedate=\'" + newTask.DueDate + "\' WHERE id=" + std::to_string(newTask.Id)+";");
+                + " WHERE id=" + std::to_string(newTask.Id)+";");
     return true;
 }
     
 bool TaskRepo::CreateTask(const Task& Task)
 {
-    if (!_dr->Connected())
+    if (_dr->Connected())
         return false;
     _dr->Exec("INSERT INTO db_task (board_id, name, text) VALUES (" + std::to_string(Task.BoardId) + ",\'" + Task.Name + "\'," + "\'" + Task.Text + "\');");
     return true;
@@ -24,7 +24,7 @@ bool TaskRepo::CreateTask(const Task& Task)
     
 std::vector<Task> TaskRepo::GetAllTasksForBoard(int boardId)
 {
-    if (!_dr->Connected())
+    if (_dr->Connected())
         std::runtime_error("Database is unavailable");
     auto answer = _dr->Exec("SELECT * FROM db_task WHERE board_id =" + std::to_string(boardId)+";"); 
     std::vector<Task> res;
@@ -38,16 +38,16 @@ std::vector<Task> TaskRepo::GetAllTasksForBoard(int boardId)
 
 bool TaskRepo::ChangeTaskStatus(TaskStatus status, int taskId)
 {
-    if (!_dr->Connected())
+    if (_dr->Connected())
         return false;
-    _dr->Exec("UPDATE db_task SET status=" + std::to_string(status) + "WHERE id=" + std::to_string(taskId)+";");
+    _dr->Exec("UPDATE db_task SET status=" + std::to_string(static_cast<int>(status)) + " WHERE id=" + std::to_string(taskId)+";");
     return true;
 }
 
 bool TaskRepo::DeleteTask(int taskId)
 {
-    if (!_dr->Connected())
+    if (_dr->Connected())
         return false;
-    _dr->Exec("DELETE FROM db_task WHERE id = " + std::to_string(2)+";");	
+    _dr->Exec("DELETE FROM db_task WHERE id=" + std::to_string(taskId)+";");	
     return true;
 }
