b3ef84dc6f10fcaf1c622b79b61c369b2d4e0fbc
Maxim Sikanov <max.4251474@yandex.ru>
2023-05-16 17:08:38 +0300

    fix sql

--- TaskMaster/src/User/UserRepo.cpp
+++ TaskMaster/src/User/UserRepo.cpp
@@ -7,7 +7,7 @@ User UserRepo::GetUser(int userId)
 {
     if (!_dr->Connected())
         throw std::runtime_error("Database is unavailable");
-    auto answer = _dr->Exec("SELECT user.id, user.name FROM \"user\" WHERE user.id=" + std::to_string(userId) + ";");
+    auto answer = _dr->Exec("SELECT * FROM \"user\" WHERE id=" + std::to_string(userId) + ";");
     if (answer.size() == 0)
         throw std::runtime_error("Users with userId=" + std::to_string(userId) + " not found");
     return serializationUser(answer[0]);
@@ -17,7 +17,7 @@ User UserRepo::GetUserByIdentity(int userIdentityId)
 {
     if (!_dr->Connected())
         throw std::runtime_error("Database is unavailable");
-    auto answer = _dr->Exec("SELECT user.id, user.name FROM \"user\" WHERE user.auth_identity_id=" + std::to_string(userIdentityId) + ";");
+    auto answer = _dr->Exec("SELECT * FROM \"user\" WHERE auth_identity_id=" + std::to_string(userIdentityId) + ";");
     if (answer.size() == 0)
         throw std::runtime_error("Users with auth_identity_id=" + std::to_string(userIdentityId) + " not found");
     return serializationUser(answer[0]);
@@ -26,7 +26,7 @@ User UserRepo::GetUserByIdentity(int userIdentityId)
 bool UserRepo::CreateUserWithIdentity(int userIdentityId)
 {
     if (!_dr->Connected())
-        throw std::runtime_error("Database is unavailable");
+        return false;
     _dr->Exec("INSERT INTO \"user\" (name, auth_identity_id) SELECT auth_identity.identity, auth_identity.id FROM auth_identity WHERE auth_identity.id =" + std::to_string(userIdentityId) + ";");
     return true;
 }
