69c84f1cffc038c2eff573a8047ae4767c96c1c9
Max425 <max.4251474@yandex.ru>
2023-05-26 14:31:38 +0300

    form for create project

--- view/src/MainPage.C
+++ view/src/MainPage.C
@@ -1,5 +1,18 @@
 #include "MainPage.h"
 
+#include <Wt/WDialog.h>
+#include <Wt/WApplication.h>
+#include <Wt/WBreak.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WDialog.h>
+#include <Wt/WEnvironment.h>
+#include <Wt/WLabel.h>
+#include <Wt/WLineEdit.h>
+#include <Wt/WTextArea.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WRegExpValidator.h>
+#include <Wt/WText.h>
+
 MainPage::MainPage(Session &session) : session_(session)
 {
     setTemplateText(tr("main-page"));
@@ -7,11 +20,7 @@ MainPage::MainPage(Session &session) : session_(session)
     user_ = session.user();
     createButton_ = this->bindWidget("createProjectButton", std::make_unique<Wt::WPushButton>("createProject"));
     createButton_->clicked().connect(this, &MainPage::createProject);
-    projectTitle_ = this->bindWidget("projectNameInput", std::make_unique<Wt::WLineEdit>());
-    projectDescription_ = this->bindWidget("projectTextInput", std::make_unique<Wt::WLineEdit>());
     createButton_->addStyleClass("btn btn-success");
-    projectTitle_->addStyleClass("form-control");
-    projectDescription_->addStyleClass("form-control");
     showProjects();
 }
 
@@ -24,19 +33,68 @@ void MainPage::showProjects()
 
 void MainPage::createProject()
 {
-    if (projectTitle_->text().empty() || projectDescription_->text().empty())
-    {
-        Wt::WMessageBox::show("Error", "Please fill in all the fields.", Wt::StandardButton::Ok);
-        return;
-    }
-    int id = session_.user().Id;
-    auto name = projectTitle_->text().toUTF8();
-    auto text = projectDescription_->text().toUTF8();
-    auto newProject = session_.mainPadgeController().CreateProject(id, name, text);
-    showProject(newProject);
-    projectTitle_->setText("");
-    projectDescription_->setText("");
+    Wt::WDialog *dialog = this->addChild(std::make_unique<Wt::WDialog>("Create new project"));
+
+    Wt::WLabel *titleLabel = dialog->contents()->addNew<Wt::WLabel>("Project title");
+    Wt::WLineEdit *titleEdit = dialog->contents()->addNew<Wt::WLineEdit>();
+    Wt::WLabel *descriptionLabel = dialog->contents()->addNew<Wt::WLabel>("Description");
+    Wt::WTextArea *descriptionEdit = dialog->contents()->addNew<Wt::WTextArea>();
+
+    titleLabel->setBuddy(titleEdit);
+    descriptionLabel->setBuddy(descriptionEdit);
+
+    dialog->contents()->addStyleClass("form-group");
+
+    auto validator = std::make_shared<Wt::WRegExpValidator>("^(?!\\s*$).+");
+    validator->setMandatory(true);
+    titleEdit->setValidator(validator);
+    descriptionEdit->setValidator(validator);
+
+    Wt::WPushButton *ok =
+        dialog->footer()->addNew<Wt::WPushButton>("Create");
+    ok->addStyleClass("btn");
+    ok->addStyleClass("btn-secondary");
+    ok->setDefault(true);
+    if (wApp->environment().ajax())
+        ok->disable();
+
+    Wt::WPushButton *cancel =
+        dialog->footer()->addNew<Wt::WPushButton>("Cancel");
+    cancel->addStyleClass("btn");
+    cancel->addStyleClass("btn");
+    dialog->rejectWhenEscapePressed();
+
+    titleEdit->keyWentUp().connect([=] {
+        ok->setDisabled(titleEdit->validate() != Wt::ValidationState::Valid);
+    });
+
+    descriptionEdit->keyWentUp().connect([=] {
+        ok->setDisabled(descriptionEdit->validate() != Wt::ValidationState::Valid);
+    });
+
+    ok->clicked().connect([=] {
+        if (titleEdit->validate() == Wt::ValidationState::Valid && descriptionEdit->validate() == Wt::ValidationState::Valid)
+            dialog->accept();
+    });
+
+    cancel->clicked().connect(dialog, &Wt::WDialog::reject);
+
+    dialog->finished().connect([=] {
+        if (dialog->result() == Wt::DialogCode::Accepted) {
+            if (titleEdit->validate() == Wt::ValidationState::Valid && descriptionEdit->validate() == Wt::ValidationState::Valid) {
+                int id = session_.user().Id;
+                auto name = titleEdit->text().toUTF8();
+                auto text = descriptionEdit->text().toUTF8();
+                auto newProject = session_.mainPadgeController().CreateProject(id, name, text);
+                showProject(newProject);
+            }
+        }
+        this->removeChild(dialog);
+    });
+
+    dialog->show();
 }
+
 
 void MainPage::showProject(const Project &proj)
 {
